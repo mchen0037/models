1f5edfa3e3583e5d683ef2306f4e8c2b459fce98
Sabrina Lauv <mslauv@gmail.com>
2020-03-31 22:35:35 -0700

    dropdown changes iframe src

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
-console.log("Hello World")
+function chooseFrame(){
+    var el = document.getElementById('iframe-dropdown');
+    var option = el.selectedIndex;
+    switch(option){
+        case 0:
+            document.getElementById('iframe-content').src="models/SampleModel.html";
+            break;
+        case 1:
+            document.getElementById('iframe-content').src="models/Fire Simple.html";
+            break;
+        case 2:
+            document.getElementById('iframe-content').src="models/Lattice Land - Explore.html";
+            break;
+
+    }
+}
+
+window.onload = function funct(){
+    chooseFrame();
+}
