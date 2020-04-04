5d080b71e6fd06d585a409c824e9519fb95a588d
Sabrina Lauv <mslauv@gmail.com>
2020-04-04 01:59:23 -0700

    reads json format and creates option list from info

--- script.js
+++ script.js
@@ -1,8 +1,38 @@
+function getiFrames(){
+    var iframeDrop = document.getElementById("iframe-dropdown");
+
+    var response = [{"id":1,"name":"Fire Simple.html"},{"id":2,"name":"Lattice Land - Explore.html"},{"id":3,"name":"SampleModel.html"}] ;
+
+for (i = 0; i < response.length; i++) 
+{ 
+	var newOption = document.createElement("option");  // Create a <option> node
+	var newOptionName = document.createTextNode(response[i].name.split(".")[0]);
+	newOption.appendChild(newOptionName); 
+	iframeDrop.appendChild(newOption); ///append Item
+}
+
+/*
+    dir = document.querySelector("/models/*.html");
+    var reader = new FileReader;
+    file = dir.target.file;
+    var newFrame;
+    var iFrameList = document.getElementById("iframe-dropdown");
+    reader.onload = (function(file){
+        reader.readAsText(file);
+        var lines = file.split('/n');
+        for(var i = 0; i < lines.length; i++){
+            newFrame.document.createElement("option");
+            newFrame.text = lines[i].split(".")[0];
+            iFrameList.appendChild(newFrame).createTextNode(newFrame.text);
+        }
+    })
+    */
+}
 function chooseFrame(){
     var el = document.getElementById('iframe-dropdown');
     var option = el.selectedIndex;
     switch(option){
-        case 0:
+        case 3:
             document.getElementById('iframe-content').src="models/SampleModel.html";
             break;
         case 1:
@@ -16,5 +46,7 @@ function chooseFrame(){
 }
 
 window.onload = function funct(){
+    getiFrames();
     chooseFrame();
+
 }
